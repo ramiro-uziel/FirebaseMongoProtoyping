b4e77f8b8fba177dcdbfd668c1f4a584e561c505
ramiro-uziel <ramirouziel15@gmail.com>
2024-09-28 17:50:24 -0600

    Vista de verificacion de correo

---
+++ FirebaseMongoProtoyping/EmailVerificationView.swift
@@ -0,0 +1,18 @@
+//
+//  EmailVerificationView.swift
+//  FirebaseMongoProtoyping
+//
+//  Created by Ramiro Uziel Rodriguez Pineda on 28/09/24.
+//
+
+import SwiftUI
+
+struct EmailVerificationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmailVerificationView()
+}
